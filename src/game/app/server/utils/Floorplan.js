161705f5fb89b650a189d1a3092da2458aa62de2
annivie <vielsack@kit.edu>
2021-06-18 10:02:46 +0200

    Add tiny robot

--- src/game/app/server/utils/Floorplan.js
+++ src/game/app/server/utils/Floorplan.js
@@ -410,6 +410,7 @@ const Floorplan = Object.freeze({
             {type: GameObjectType.FLOORDECORATION, position: [11,20], variation: 2},
             {type: GameObjectType.FLOORDECORATION, position: [12,20], variation: 3},
             {type: GameObjectType.FLOORDECORATION, position: [13,20], variation: 4},
+            {type: GameObjectType.FLOORDECORATION, position: [14,20], variation: 5},
             
             {type: GameObjectType.TABLE, position: [0,24],  isClickable: true, variation: 1},
             {type: GameObjectType.TABLE, position: [[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22],24],  isClickable: true, variation: 2},
